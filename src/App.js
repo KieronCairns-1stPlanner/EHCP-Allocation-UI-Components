1642cc4f146a9342e4ef42cde101722ff5f0f131
Kieron Cairns <kieroncairns@protonmail.com>
2023-12-30 17:49:51 +0000

    CHANGE: User can select a list item. Currently this will only be the School name

--- src/App.js
+++ src/App.js
@@ -3,34 +3,34 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import { links } from "./data/links.js"
 
-const List = props => {
-  return props.list.map(item => (
-      <div key={item.objectID}>
-      <span>
-        {item.EstablishmentName}
-      </span>
-
+const List = ({ list, onSelectItem }) => {
+  return list.map(item => (
+    <div key={item.objectID} onClick={() => onSelectItem(item)}>
+      <span>{item.EstablishmentName}</span>
       <span>{item.Street}</span>
       <span>{item.Town}</span>
       <span>{item.Countyname}</span>
-      </div>
+    </div>
   ));
-}
+};
 
-const Search = ({ onSearch, onFocus, onBlur }) => {
-  const [searchTerm, setSearchTerm] = React.useState('');
+
+const Search = ({ searchTerm, onSearch, onFocus, onBlur }) => {
+  // The internal state is no longer necessary since the value is controlled by the App component
+  // const [searchTerm, setSearchTerm] = React.useState('');
 
   const handleChange = event => {
-    setSearchTerm(event.target.value);
+    // setSearchTerm(event.target.value); // This line is no longer necessary
     onSearch(event);
   };
-  
+
   return (
     <div>
       <label htmlFor='search'>Search: </label>
       <input 
         id="search" 
         type='text' 
+        value={searchTerm} // Controlled by the App component
         onChange={handleChange} 
         onFocus={onFocus} 
         onBlur={onBlur} 
@@ -58,6 +58,11 @@ const App = () => {
     setIsInputFocused(false);
   };
 
+  const handleSelectItem = (item) => {
+    setSearchTerm(item.EstablishmentName);
+    setIsInputFocused(false); // Optionally hide the list after selection
+  };
+
   const searchedStories = links.filter(story => {
     // Split the search term into individual words and convert to lowercase
     const searchWords = searchTerm.toLowerCase().split(' ');
@@ -78,9 +83,14 @@ const App = () => {
   return (
     <div>
       <h1>Stories</h1>
-      <Search onSearch={handleSearch} onFocus={handleFocus} onBlur={handleBlur} />
-      <hr />
-      {isInputFocused && searchTerm.length > 1 && <List list={searchedStories} />}
+      <Search 
+  searchTerm={searchTerm}
+  onSearch={handleSearch} 
+  onFocus={handleFocus} 
+/>      <hr />
+      {isInputFocused && searchTerm.length > 1 && (
+        <List list={searchedStories} onSelectItem={handleSelectItem} />
+      )}
     </div>  
   );
 }
